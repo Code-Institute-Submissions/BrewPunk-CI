ce8ea776f370ce5f8ec7a060a6ca9e1b91b9b6c9
Dermot Murphy <dermotmurphy@Dermots-MacBook-Pro.local>
2020-08-03 18:26:38 +0100

    added basic logic for ABV calculator |  Functions added for Fetch API |Footer updated with social links and page links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,34 @@
 
+  let ABVValue = document.getElementById("ABVSlider").value;
+  let IBUValue = document.getElementById("IBUSlider").value;
+
+//Set Button values based on slider
+document.getElementById("abv-slider-value").innerHTML = ABVValue;
+document.getElementById("ibu-slider-value").innerHTML = IBUValue;
+
+
+function updateABVButton() {
+  let ABVValue = document.getElementById("ABVSlider").value;
+  document.getElementById("abv-slider-value").innerHTML =  ABVValue;
+}
+
+function updateIBUButton() {
+  let IBUValue = document.getElementById("IBUSlider").value;
+  document.getElementById("ibu-slider-value").innerHTML =  IBUValue;
+}
 //Fetch Data from punkAPI
 
-async function getData(){
-    await fetch('https://api.punkapi.com/v2/beers')
+async function getDataABV(){
+
+  console.log(ABVValue);
+  console.log(typeof(IBUValue));
+ // sample API command  https://api.punkapi.com/v2/beers?page=2&per_page=80
+    await fetch(`https://api.punkapi.com/v2/beers?` + `abv_gt=${ABVValue}`+ `&per_page=80`)
       .then(response => response.json())
-      .then(data => console.log(data[0].name));
+      .then(data => console.log(data));
        
     }
     
-    getData();
 
  //Calculators Section
  document.addEventListener('DOMContentLoaded', function() {
@@ -30,5 +50,6 @@ function resetABV(){
     document.getElementById("initialGravity").value = "";
     document.getElementById("finalGravity").value = "";
     document.getElementById("ABVResult").value = "";
+    alert('ABV Calculator Reset');
 
 }
